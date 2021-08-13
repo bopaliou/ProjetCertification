7d0f6d42880db370b590c04424787e1676907f89
Aliou Bop <bopaliou1994@gmail.com>
2021-08-13 19:03:59 +0200

    mise en place de l'environnement docker

--- .history/public/index_20210813190216.php
+++ .history/public/index_20210813190216.php
@@ -3,7 +3,7 @@
 use App\Kernel;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
-dd($_SERVER);
+dd($_S)
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };

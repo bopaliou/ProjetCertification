5a864d76701e8d600b5abc7d31eaa46b87c3c6af
Aliou Bop <bopaliou1994@gmail.com>
2021-08-13 21:05:13 +0200

    un peu de style

--- .history/src/Controller/RegistrationController_20210813204215.php
+++ .history/src/Controller/RegistrationController_20210813204215.php
@@ -12,7 +12,7 @@ use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class RegistrationController extends AbstractController
 {
-    #[Route('/inscription', name: 'app_register')]
+    #[Route('/register', name: 'app_register')]
     public function register(Request $request, UserPasswordEncoderInterface $passwordEncoder): Response
     {
         $user = new User();
